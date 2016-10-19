42f4a17c20dfb4cd8b0da3ff7153052cf0a636ed
Artur Malyk <imboko2@gmail.com>
2016-10-19 15:26:13 +0300

    sc

--- js/hw7-8js.js
+++ js/hw7-8js.js
@@ -2,14 +2,14 @@ $('.button_number_one').click(function(){ $('.first_text').removeClass("hidden")
 $('.button_number_two').click(function(){ $('.second_text').removeClass("hidden");$('.first_text').addClass("hidden");$('.third_text').addClass("hidden");});
 $('.button_number_three').click(function(){ $('.third_text').removeClass("hidden");$('.first_text').addClass("hidden");$('.second_text').addClass("hidden");});
 
-$(inp1).mouseover(function(){$(h1).removeClass("hidden");});
-$(inp2).mouseover(function(){$(h2).removeClass("hidden");});
-$(inp3).mouseover(function(){$(h3).removeClass("hidden");});
-$(inp1).mouseout(function(){$(h1).addClass("hidden");});
-$(inp2).mouseout(function(){$(h2).addClass("hidden");});
-$(inp3).mouseout(function(){$(h3).addClass("hidden");});
+$(inp1).click(function(){$(h1).toggle(h1);});
+$(inp2).click(function(){$(h2).toggle(h2);});
+$(inp3).click(function(){$(h3).toggle(h3);});
+// $(inp1).mouseout(function(){$(h1).toggle(h1);});
+// $(inp2).mouseout(function(){$(h2).toggle(h2);});
+// $(inp3).mouseout(function(){$(h3).toggle(h3);});
 
-$(b1).click(function(){$(h1).removeClass("hidden");
-    $(h2).removeClass("hidden");
-    $(h3).removeClass("hidden");});
+$(b1).click(function()  {$(h1).toggle(h1);
+                        $(h2).toggle(h2);
+                        $(h3).toggle(h3);});
 
